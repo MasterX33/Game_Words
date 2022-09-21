e922773fe57a9fc4bb6214669c519c36bd2620c7
Anton Krotov <masterxroot@gmail.com>
2022-09-21 17:17:07 +0300

    1-st commit Game Words

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+# Game WORDS
+
+igroki = []
+words = []
+
+players = None
+
+def player(): # функция которая отвечает за создание игроков
+    while True:
+        players = input("Введите имя игрока: ")
+        if players == "Start":
+            print("Сейчас играют: ", igroki)
+            print(" \n Начало игры \n")
+            break
+        else:
+            igroki.append(players)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
+def game_start(): # финкционал игры, сохранение в список введеных слов, проверка на повторение, проверка первой и последней буквы в словах
+    while True:
+        for i in igroki:
+            word = input()
+            print("Игрок ", i, "Ввел слово ", word)
+            while word in words:
+                if word in words:
+                    print(i, "это слово уже вводилось, введите другое")
+                    word = input()
+                elif word == "exit":
+                    print("Конец игры!")
+                    return 0
+                else:
+                    words.append(word)
+                    break
+
+            words.append(word)
+
+
+player()
+game_start()
